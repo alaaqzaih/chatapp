98145381b318415b969185485502afa8f1765cfe
alaaqz <alaa.qzaih@gmail.com>
2022-07-15 02:09:27 +0300

    first commit

---
+++ chat_app/lib/Methods.dart
@@ -0,0 +1,63 @@
+//import 'package:chat_app/Authenticate/LoginScree.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'LoginScreen.dart';
+
+Future<User?> createAccount(String name, String email, String password) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  try {
+    User? user = (await _auth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (user != null) {
+      print("Account created Successfull");
+      return user;
+    } else {
+      print("account failed");
+      return user;
+    }
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
+
+Future<User?> logIn(String email, String password) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  // FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  try {
+    User? user = (await _auth.signInWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (user != null) {
+      print("log Successfull");
+      return user;
+    } else {
+      print("log failed");
+      return user;
+    }
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
+
+Future logOut(BuildContext context) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  try {
+    await _auth.signOut().then((value) {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+    });
+  } catch (e) {
+    print("error");
+  }
+}
